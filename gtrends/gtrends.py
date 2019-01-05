2491e2ccc4cdd1153a3b5346da73d0cdae34bdaa
Adam McCreevey <mccreeap@tcd.ie>
2019-01-05 16:55:25 +0000

    Next Commit

--- gtrends/gtrends.py
+++ gtrends/gtrends.py
@@ -1,11 +1,24 @@
 from pytrends.request import TrendReq
 import pandas as pd
 
-pytrends = TrendReq(hl='en-UK', tz=360)
 
-kw_list = ["Electroneum"]
+def returndict(kw_list):
+    pytrends = TrendReq(hl='en-US', tz=360)
 
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-df = pytrends.interest_over_time()
+    df_kw_list = dict()
 
-print(df.head())
+    for item in kw_list:
+        pytrends.build_payload([item], cat=0, timeframe='today 3-m', geo='', gprop='')
+        df_kw_list[item] = pytrends.interest_over_time().drop(columns="isPartial")
+
+    return df_kw_list
+
+
+dFrames = returndict(["Electroneum", "Bitcoin", "Ethereum"])
+
+print(dFrames["Electroneum"].loc[dFrames["Electroneum"].idxmax()].index.get_values())
+print(dFrames["Bitcoin"].loc[dFrames["Bitcoin"].idxmax()])
+print(dFrames["Bitcoin"].loc[dFrames["Bitcoin"].idxmax()].index.get_values())
+print(dFrames["Ethereum"].loc[dFrames["Ethereum"].idxmax()])
+print(dFrames["Ethereum"].loc[dFrames["Ethereum"].idxmax()].index.get_values())
+
