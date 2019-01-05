90ac5ae27136f84e7195abeaf94fc4e4a5ec696f
Adam McCreevey <mccreeap@tcd.ie>
2019-01-05 13:58:57 +0000

    First Commit

--- gtrends/gtrends.py
+++ gtrends/gtrends.py
@@ -1,3 +1,11 @@
-# -*- coding: utf-8 -*-
+from pytrends.request import TrendReq
+import pandas as pd
 
-"""Main module."""
+pytrends = TrendReq(hl='en-UK', tz=360)
+
+kw_list = ["Electroneum"]
+
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+df = pytrends.interest_over_time()
+
+print(df.head())
